780880ddd6f43274c185ea85f7fb14b5a11ba532
Juan Carlos Cancela <bak@Juans-MacBook-Pro-3.local>
2020-04-27 01:40:46 -0300

    phaser test

---
+++ precache-manifest.8e965cffed3036fa2e602bb745081291.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638a662c38bfedc9255214cc253395e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07afd95283698ab6aee",
+    "url": "/static/css/main.e8de8aff.chunk.css"
+  },
+  {
+    "revision": "16ffa68679ed50bc2dd5",
+    "url": "/static/js/2.b40c0f1c.chunk.js"
+  },
+  {
+    "revision": "6ea9a6dc6e39fe7dbb9b2fd540d08211",
+    "url": "/static/js/2.b40c0f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07afd95283698ab6aee",
+    "url": "/static/js/main.4f2f2115.chunk.js"
+  },
+  {
+    "revision": "abfb520540b962e50ea5",
+    "url": "/static/js/runtime-main.19f8a914.js"
+  }
+]);
